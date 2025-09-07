8a857f45f32fff8b110e4af54d22bf83fb497187
cgartco6 <155203611+cgartco6@users.noreply.github.com>
2025-09-07 22:11:24 +0200

    Create Game Engine.swift

---
+++ ios/Ghosts/Game Engine.swift
@@ -0,0 +1,27 @@
+import SpriteKit
+
+class GameEngine: SKScene {
+    override func didMove(to view: SKView) {
+        // Setup your game scene here
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+        // Called before each frame is rendered
+    }
+    
+    func startGame() {
+        // Start the game loop
+    }
+    
+    func stopGame() {
+        // Stop the game loop
+    }
+    
+    func updateGame() {
+        // Update game state
+    }
+    
+    func renderGame() {
+        // Render game elements
+    }
+}
